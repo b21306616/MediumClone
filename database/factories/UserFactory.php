ae34706eeff17f4e1deb41d84b0c5a46b7513578
b21306616 <b21306616@cs.hacettepe.edu.tr>
2026-08-19 10:49:27 +0500

    refactor(factory): comment out two-factor authentication attributes and method
    - Comment out two-factor authentication related attributes in the default state
    - Comment out the withTwoFactor method to prevent automatic configuration of two-factor authentication

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -35,9 +35,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'two_factor_confirmed_at' => null,
+            // 'two_factor_secret' => null,
+            // 'two_factor_recovery_codes' => null,
+            // 'two_factor_confirmed_at' => null,
         ];
     }
 
@@ -54,12 +54,12 @@ class UserFactory extends Factory
     /**
      * Indicate that the model has two-factor authentication configured.
      */
-    public function withTwoFactor(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'two_factor_secret' => encrypt('secret'),
-            'two_factor_recovery_codes' => encrypt(json_encode(['recovery-code-1'])),
-            'two_factor_confirmed_at' => now(),
-        ]);
-    }
+    // public function withTwoFactor(): static
+    // {
+    //     return $this->state(fn (array $attributes) => [
+    //         'two_factor_secret' => encrypt('secret'),
+    //         'two_factor_recovery_codes' => encrypt(json_encode(['recovery-code-1'])),
+    //         'two_factor_confirmed_at' => now(),
+    //     ]);
+    // }
 }
